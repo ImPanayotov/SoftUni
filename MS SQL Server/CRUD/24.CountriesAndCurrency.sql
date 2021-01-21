c4e95ba0dd3c65af28d1c50b680241e3af30daee
Ivo M. Panayotov <i.m.panayotov@gmail.com>
2021-01-21 03:14:05 +0200

    MSSQL CRUD

---
+++ MS SQL Server/CRUD/24.CountriesAndCurrency.sql
@@ -0,0 +1,10 @@
+SELECT CountryName, CountryCode,
+	CASE
+		WHEN CurrencyCode = 'Eur' THEN 'Euro'
+		ELSE 'Not Euro'
+	END  AS 'Currency'
+FROM Countries
+ORDER BY CountryName
+
+
+SELECT * FROM Countries
