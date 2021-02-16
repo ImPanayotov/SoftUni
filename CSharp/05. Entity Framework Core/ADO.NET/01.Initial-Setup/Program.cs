75fe0e9837919ecd3b2f93b43b59e0863b0559ec
Ivo M. Panayotov <i.m.panayotov@gmail.com>
2021-02-16 21:57:36 +0200

    Update Program.cs

--- CSharp/05. Entity Framework Core/ADO.NET/01.Initial-Setup/Program.cs
+++ CSharp/05. Entity Framework Core/ADO.NET/01.Initial-Setup/Program.cs
@@ -7,10 +7,31 @@ namespace _01.Initial_Setup
     {
         static void Main(string[] args)
         {
-            string connectionString = "Server=IMPANAYOTOV\\SQLEXPRESS; Integrated Security = true; Database = Softuni";
-            var connection = new SqlConnection(connectionString);
-            connection.Open();
-            var query = new SqlCommand("SELECT COUNT(*) FROM Employees");
+            string connectionString = "Server=IMPANAYOTOV\\SQLEXPRESS; Integrated Security = true; Database = SoftUni";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string queryStr = "UPDATE Employees SET Salary += 0.12";
+                SqlCommand sqlCommand = new SqlCommand(queryStr, connection);
+
+                var rowsAffected = sqlCommand.ExecuteNonQuery();
+
+                Console.WriteLine(rowsAffected);
+            }
+
+            //SqlConnection dbCon = new SqlConnection("Server=IMPANAYOTOV\\SQLEXPRESS; Integrated Security = true; Database = SoftUni");
+            //dbCon.Open();
+            //using (dbCon)
+            //{
+            //    SqlCommand command = new SqlCommand(
+            //      "SELECT COUNT(*) FROM Employees", dbCon);
+            //    int employeesCount = (int)command.ExecuteScalar();
+            //    Console.WriteLine("Employees count: {0} ", employeesCount);
+            //}
+
+
         }
     }
 }
